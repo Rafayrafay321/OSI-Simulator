2cb4ff62a9ba7308f40dd43644b5b6251771dc4c
Rafayrafay321 <alihaidersh72@gmail.com>
2026-04-16 21:55:18 +0500

    Added ws for webSocket and created simple wss

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -1,12 +1,34 @@
-// Custom imports
+// Node Imports
+import * as http from 'node:http';
+import WebSocket from 'ws';
+
+// Custom Imports
 import app from '../app';
 import { env } from '../config/env';
 
 const PORT = env.PORT || 3001;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.send('Welcome to PacketForge!');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+
+    ws.send('Hello from server');
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
 
 const startServer = () => {
   try {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is firing on http://localhost:${PORT}`);
     });
   } catch (error) {
